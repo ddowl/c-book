981d797c5b4222ac15ff648907a420433a9310de
Drew Dowling <drewdowling37@gmail.com>
2018-10-01 23:43:23 -0700

    finished remove comments

--- 1/remove-comments.c
+++ 1/remove-comments.c
@@ -32,14 +32,46 @@ int consumeLine(char s[], int lim) {
   return lineLen;
 }
 
+int isInlineComment(char s[], int start) {
+  return s[start] == '/' && s[start + 1] == '/';
+}
+
+int isMultilineCommentStart(char s[], int start) {
+  return s[start] == '/' && s[start + 1] == '*';
+}
+
+int isMultilineCommentEnd(char s[], int start) {
+  return s[start] == '*' && s[start + 1] == '/';
+}
+
 // removes all comments from a C program.
 // Don't forget to handle quoted strings and character constants properly.
 int main() {
   int len = 0;            // current line length
   char line[MAXLINE];     // current input line
 
+  int inComment = 0;
   while ((len = consumeLine(line, MAXLINE)) > 0) {
+    for (int i = 0; i < len; i++) {
+      if (isInlineComment(line, i)) {
+        printf("\n");
+        break; // leave for loop, finish processing this line
+      }
 
+      if (!inComment) {
+        if (isMultilineCommentStart(line, /* clever hidden comment */ i)) {
+          inComment = 1;
+          i += 1; // so we'll start looking for the multiline comment end after the /*
+          continue;
+        }
+        putchar(line[i]);
+      } else {
+        if (isMultilineCommentEnd(line, i)) {
+          inComment = 0;
+          i += 1;
+        }
+      }
+    }
   }
   return 0;
 }
