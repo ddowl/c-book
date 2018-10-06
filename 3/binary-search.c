ae78a0e9b4216eb7fe67847beea63884aea55ea0
Drew Dowling <drewdowling37@gmail.com>
2018-10-05 21:52:58 -0700

    Ch3 binary-search and escape

--- 3/binary-search.c
+++ 3/binary-search.c
@@ -4,6 +4,9 @@
   with only one test inside the loop and measure the difference in run-time.
 */
 
+#include <time.h>
+#include <stdio.h>
+
 int binsearch(int x, int v[], int n) {
   int low, high, mid;
 
@@ -23,9 +26,50 @@ int binsearch(int x, int v[], int n) {
 }
 
 int fastBinsearch(int x, int v[], int n) {
+  int low, high, mid;
 
+  low = 0;
+  high = n - 1;
+  mid = (low+high) / 2;
+
+  while ((low <= high) && (v[mid] != x)) {
+    if (x < v[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+    mid = (low+high) / 2;
+  }
+  return (v[mid] == x) ? mid : -1;
 }
 
-int main() {
+void measureFunc(int (*func)(int, int[], int), int arr[], int len) {
+  // Calculate the time taken by func()
+  clock_t t;
+  t = clock();
+  for (int i = 0; i < len; i++) {
+    (*func)(i, arr, len);
+  }
+  t = clock() - t;
+  double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
 
+  printf("func() took %f seconds to execute \n", time_taken);
+}
+
+
+int main() {
+  int len = 1000000;
+  int arr[len];
+  for (int i = 0; i < len; i++) {
+    arr[i] = i;
+  }
+
+  int x = 1234;
+  printf("binsearch(%d) = %d\n", x, binsearch(x, arr, len));
+  printf("fastBinsearch(%d) = %d\n", x, fastBinsearch(x, arr, len));
+
+  printf("Timing binsearch...\n");
+  measureFunc(binsearch, arr, len);
+  printf("Timing fastBinsearch...\n");
+  measureFunc(fastBinsearch, arr, len);
 }
--- 3/escape.c
+++ 3/escape.c
@@ -2,17 +2,59 @@
   Write a function escape(s, t) that converts characters like newline
   and tab into visible escape sequences like \n and \t as it copies the
   string t to s. Use a switch. Write a function for the other direction as well,
-  converting escape sequences into the real characters. 
+  converting escape sequences into the real characters.
 */
 
-void escape(char s[], char t[]) {
+#include <stdio.h>
+#include <string.h>
+#define SUFFICIENT_BUFFER_LEN 100
 
+void escape(char src[], char dest[]) {
+  int i, j;
+  for(i = j = 0; i < strlen(src); i++, j++) {
+    if (src[i] == '\n') {
+      dest[j] = '\\';
+      dest[j+1] = 'n';
+      j++;
+    } else if (src[i] == '\t') {
+      dest[j] = '\\';
+      dest[j+1] = 't';
+      j++;
+    } else {
+      dest[j] = src[i];
+    }
+  }
+  dest[j] = '\0';
 }
 
-void unescape(char s[], char t[]) {
-  
+void unescape(char src[], char dest[]) {
+  int i, j;
+  for(i = j = 0; i < strlen(src); i++, j++) {
+    if (src[i] == '\\') {
+      if (src[i+1] == 'n') {
+        dest[j] = '\n';
+        i++;
+      } else if (src[i+1] == 't') {
+        dest[j] = '\t';
+        i++;
+      } else {
+        dest[j] = src[i]; // unfortunate duplication :(
+      }
+    } else {
+      dest[j] = src[i];
+    }
+  }
+  dest[j] = '\0';
 }
 
 int main() {
+  char escapes[] = "\n\t\n\n\t";
+  char literals[] = "\\n\\t\\n\\n\\t";
+  char escapeDest[SUFFICIENT_BUFFER_LEN];
+  char literalDest[SUFFICIENT_BUFFER_LEN];
 
+  escape(escapes, escapeDest);
+  unescape(literals, literalDest);
+  printf("escape(%s) = %s\n", escapes, escapeDest);
+  printf("unescape(%s) = %s\n", literals, literalDest);
 }
