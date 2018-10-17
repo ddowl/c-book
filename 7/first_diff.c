5f5d85b986d4f491fe3c8aac23fe2565dbab0e4e
Drew Dowling <drewdowling37@gmail.com>
2018-10-16 21:16:22 -0700

    added starters for ch7

---
+++ 7/first_diff.c
@@ -0,0 +1,3 @@
+/*
+  Write a program to compare two files, printing the first time where they differ.
+*/
---
+++ 7/isupper.c
@@ -0,0 +1,4 @@
+/*
+  Functions like isupper can be implemented to save space or to save time.
+  Explore both possibilities.
+*/
